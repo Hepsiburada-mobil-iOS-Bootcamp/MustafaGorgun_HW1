999a68ff2e04755e539c6135ae572163d999a533
MustafaGrgn <mustafagorgun87@hotmail.com>
2021-09-24 20:12:34 +0300

    Arrangement Module Added

---
+++ iOS_Week_1/Modules/SkillsStruct.swift
@@ -0,0 +1,12 @@
+//
+//  SkillsStruct.swift
+//  iOS_Week_1
+//
+//  Created by MstfGrgn on 24.09.2021.
+//
+
+import Foundation
+struct SkillsStruct {
+    let attack: String
+    let defence: String
+}
